b639c1332ad0b1efa59a71939c82d11168d416f4
nacho_torres_1123@hotmail.com <43484201+nachotorres1123@users.noreply.github.com>
2023-08-13 18:32:54 -0400

    Update main.php

--- main.php
+++ main.php
@@ -1,136 +1,83 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Bot Token
+|--------------------------------------------------------------------------
+|
+| Change this to your Bot API Token
+| It can be obtained from https://telegram.dog/BotFather
+|
+*/
+$config['botToken'] = "6687375790:AAFiNosM8x-RhB0GZQrE-9l9BjUBS-zKpSk";
 
-include __DIR__."/config/config.php";
-include __DIR__."/config/variables.php";
-include __DIR__."/functions/bot.php";
-include __DIR__."/functions/functions.php";
-include __DIR__."/functions/db.php";
+/*
+|--------------------------------------------------------------------------
+| Admin User IDs
+|--------------------------------------------------------------------------
+|
+| Change this to Admin's Numeric User IDs
+| IDs can be obtained from https://telegram.dog/username_to_id_bot
+|
+*/
+$config['adminIDs'] = ["6175765051", "6687375790"];
 
+/*
+|--------------------------------------------------------------------------
+| Logs Channel ID
+|--------------------------------------------------------------------------
+|
+| Create a New Channel/Group for logging data
+| ID can be obtained from https://telegram.dog/BotFather
+|
+*/
+$config['logsID'] = "1001908451090";
 
-date_default_timezone_set($config['timeZone']);
+/*
+|--------------------------------------------------------------------------
+| Timezone
+|--------------------------------------------------------------------------
+|
+| Current timezone for Logging Activities with time
+| It can be obtained from http://1min.in/content/international/time-zones
+| By Default, it's in IST
+|
+*/
+$config['timeZone'] = "Asia/Kolkata";
 
+/*
+|--------------------------------------------------------------------------
+| Database
+|--------------------------------------------------------------------------
+| Database to Store User Data
+|
+*/
+$config['db']['hostname'] = "sql9.freesqldatabase.com";
+$config['db']['username'] = "sql9639006";
+$config['db']['password'] = "mGa7jjKVE9";
+$config['db']['database'] = "checkerbot";
 
-////Modules
-include __DIR__."/modules/admin.php";
-include __DIR__."/modules/skcheck.php";
-include __DIR__."/modules/binlookup.php";
-include __DIR__."/modules/iban.php";
-include __DIR__."/modules/stats.php";
-include __DIR__."/modules/me.php";
-include __DIR__."/modules/apikey.php";
+/*
+|--------------------------------------------------------------------------
+| Anti-Spam Timer
+|--------------------------------------------------------------------------
+|
+| Anti-Spam Timer to prevent Spammers from Spamming the Checker
+| Value is in Seconds. "20" = 20 seconds
+|
+*/
+$config['anti_spam_timer'] = 20;
 
-
-include __DIR__."/modules/checker/ss.php";
-include __DIR__."/modules/checker/schk.php";
-include __DIR__."/modules/checker/sm.php";
-
-
-
-//////////////===[START]===//////////////
-
-if(strpos($message, "/start") === 0){
-if(!isBanned($userId) && !isMuted($userId)){
-
-  if($userId == $config['adminID']){
-    $messagesec = "<b>Type /admin to know admin commands</b>";
-  }
-
-    addUser($userId);
-    bot('sendmessage',[
-        'chat_id'=>$chat_id,
-        'text'=>"<b>Hello @$username,
-
-Type /cmds to know all my commands!</b>
-
-$messagesec",
-	'parse_mode'=>'html',
-	'reply_to_message_id'=> $message_id,
-    'reply_markup'=>json_encode(['inline_keyboard' => [
-        [
-          ['text' => "💠 Created By 💠", 'url' => "t.me/iamNVN"]
-        ],
-        [
-          ['text' => "💎 Source Code 💎", 'url' => "GitHub.com/iam-NVN/SDMN_CheckerBot"]
-        ],
-      ], 'resize_keyboard' => true])
-        
-    ]);
-  }
-}
-
-//////////////===[CMDS]===//////////////
-
-if(strpos($message, "/cmds") === 0 || strpos($message, "!cmds") === 0){
-
-  if(!isBanned($userId) && !isMuted($userId)){
-    bot('sendmessage',[
-    'chat_id'=>$chat_id,
-    'text'=>"<b>Which commands would you like to check?</b>",
-    'parse_mode'=>'html',
-    'reply_to_message_id'=> $message_id,
-    'reply_markup'=>json_encode(['inline_keyboard'=>[
-    [['text'=>"💳 CC Checker Gates",'callback_data'=>"checkergates"]],[['text'=>"🛠 Other Commands",'callback_data'=>"othercmds"]],
-    ],'resize_keyboard'=>true])
-    ]);
-  }
-  
-  }
-  
-  if($data == "back"){
-    bot('editMessageText',[
-    'chat_id'=>$callbackchatid,
-    'message_id'=>$callbackmessageid,
-    'text'=>"<b>Which commands would you like to check?</b>",
-    'parse_mode'=>'html',
-    'reply_markup'=>json_encode(['inline_keyboard'=>[
-    [['text'=>"💳 CC Checker Gates",'callback_data'=>"checkergates"]],[['text'=>"🛠 Other Commands",'callback_data'=>"othercmds"]],
-    ],'resize_keyboard'=>true])
-    ]);
-  }
-  
-  if($data == "checkergates"){
-    bot('editMessageText',[
-    'chat_id'=>$callbackchatid,
-    'message_id'=>$callbackmessageid,
-    'text'=>"<b>━━CC Checker Gates━━</b>
-  
-<b>/ss | !ss - Stripe [Auth]</b>
-<b>/sm | !sm - Stripe [Merchant]</b>
-<b>/schk | !schk - User Stripe Merchant [Needs SK]</b>
-
-<b>/apikey sk_live_xxx - Add SK Key for /schk gate</b>
-<b>/myapikey | !myapikey - View the added SK Key for /schk gate</b>
-
-<b>ϟ Join <a href='t.me/pyLeads'>pyLeads</a></b>",
-    'parse_mode'=>'html',
-    'disable_web_page_preview'=>true,
-    'reply_markup'=>json_encode(['inline_keyboard'=>[
-  [['text'=>"Return",'callback_data'=>"back"]]
-  ],'resize_keyboard'=>true])
-  ]);
-  }
-  
-  
-  if($data == "othercmds"){
-    bot('editMessageText',[
-    'chat_id'=>$callbackchatid,
-    'message_id'=>$callbackmessageid,
-    'text'=>"<b>━━Other Commands━━</b>
-  
-<b>/me | !me</b> - Your Info
-<b>/stats | !stats</b> - Checker Stats
-<b>/key | !key</b> - SK Key Checker
-<b>/bin | !bin</b> - Bin Lookup
-<b>/iban | !iban</b> - IBAN Checker
-  
-  <b>ϟ Join <a href='t.me/pyLeads'>pyLeads</a></b>",
-    'parse_mode'=>'html',
-    'disable_web_page_preview'=>true,
-    'reply_markup'=>json_encode(['inline_keyboard'=>[
-  [['text'=>"Return",'callback_data'=>"back"]]
-  ],'resize_keyboard'=>true])
-  ]);
-  }
+/*
+|--------------------------------------------------------------------------
+| SK Keys
+|--------------------------------------------------------------------------
+|
+| SK Keys for !sm checker gate
+| Add a Live SK Key here. You can Also add Multiple SK Keys
+| Example: array('sk1', 'sk2', 'sk3')
+|
+*/
+$config['sk_keys'] = ['sk_live_69GKI0saLB8uIEnxzv8VTvRX'];
 
 ?>
